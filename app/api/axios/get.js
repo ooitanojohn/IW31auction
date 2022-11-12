c707306a4c108ac3da895a9d5d8abef02b123793
ooitanojohn <qmkh733310@gmail.com>
2022-11-12 20:52:53 +0900

    🐧 chore:

--- app/api/axios/get.js
+++ app/api/axios/get.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
-axios.get('http://127.0.0.1:180/IW13auction/temp/helloworld.php')
+axios
+  .get('http://localhost:180/IW31auction/API/temp.php')
   .then((response) => {
     console.log(response);
   })
   .catch((error) => {
     console.log(error);
   })
-  .finally(() => {
-  });
+  .finally(() => {});
