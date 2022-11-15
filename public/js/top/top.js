9f1a410527dfda34825ba7be093026a11457f86e
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2022-11-15 23:05:11 +0900

    🐧 chore: 52-ブラウザからapi叩くときのcorsの回避方法 .htaccessに追記→サーバサイドでヘッダー情報付与で行く

--- public/js/top/top.js
+++ public/js/top/top.js
@@ -1,16 +1,13 @@
-const getJson = (json) => {
-  console.log(json.name);
-  return json.name;
-};
 axios
   .get('http://localhost/IW31auctionAPI/top/helloworld.php', {
-    url: '/php',
-    adapter: axiosJsonpAdapter,
-    callbackParamName: 'getJson',
+    withCredentials: true, // cookieを送信したい場合
     params: {
       name: 'siwk',
     },
   })
   .then((res) => {
     console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
   });
