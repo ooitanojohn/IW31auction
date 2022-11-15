4035b09e3fe4c6d6173fe099aaf072db4ee312e5
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2022-11-15 15:56:10 +0900

    32-middlewareのフ ァイル分割方法 ファイル分割理解 後はpath設定で短く読み込みたい？

--- app/controller/regist/router.js
+++ app/controller/regist/router.js
@@ -1,26 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const crypto = require('crypto');
-
-// csrf middleware
-router.use((req, res, next) => {
-  const { method } = req;
-  // GEtの場合randomなtokenをhtmlに埋め込む
-  if (method === 'GET') {
-    const csrfToken = crypto.randomBytes(20).toString('hex');
-    req.session.csrfToken = csrfToken;
-    res.locals = {
-      csrfToken,
-      csrfField: `<input type="hidden" name="csrfToken" value="${csrfToken}">`,
-    };
-  } else if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(method)) {
-    if (req.body.csrfToken !== req.session.csrfToken) {
-      return res.status(419).send('Page Expired : 不正なページ更新が行われました');
-    }
-  }
-  return next();
-});
+const csrf = require('../../../config/middleware/csrf');
+//  csrf
+router.use(csrf);
 
 /* GET homePage. */
 router.get('/', (req, res) => {
