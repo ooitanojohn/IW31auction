6ac70aa9ed3d52fe60871af52b7a0f8987b53831
nakata2002 <ookinaki12@gmail.com>
2022-11-20 22:31:32 +0900

    55-メール送信

--- app/common/mail.js
+++ app/common/mail.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 // SMTP の設定
 //
 const options = {
-  host: 'smtp.gmail.com', // メールサーバー
-  port: 587, // ポート番号 25 など(gmailは587)
+  host: process.env.SMTP_HOST, // メールサーバー
+  port: process.env.SMTP_PORT, // ポート番号 25 など(gmailは587)
   secure: false, // 465 番ポートを使う場合。それ以外は false
   requireTLS: false,
   tls: {
@@ -12,8 +12,8 @@ const options = {
   },
   auth: {
     // 認証情報
-    user: 'ookinaki12@gmail.com', // ユーザー名
-    pass: 'xxxxxxxxxx', // アプリ生成パスワード（①googleアカウント→②セキュリティ→③アプリパスワードを作成→④メール、(macかwindows)か選択し生成）
+    user: process.env.SMTP_USER, // ユーザー名
+    pass: process.env.SMTP_PASS, // アプリ生成パスワード（①googleアカウント→②セキュリティ→③アプリパスワードを作成→④メール、(macかwindows)か選択し生成）
   },
 };
 
