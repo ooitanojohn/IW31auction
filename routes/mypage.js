8d9eace7757b20fa3f59db223636acd5a4006f2c
ooitanojohn <qmkh733310@gmail.com>
2022-12-04 23:54:10 +0900

    🌟 feat: マイページ完成 maxlength効かない なぜ

--- routes/mypage.js
+++ routes/mypage.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { updateAccount } = require('../app/controller/mypageController');
+const {
+  updateAccount,
+  updateAddress,
+  updateCard,
+  updateDrop,
+} = require('../app/controller/mypageController');
 const { executeQuery } = require('../app/module/mysqlPool');
 const { httpRapper } = require('../app/common/httpRapper');
 
@@ -25,6 +30,7 @@ router.get('/', async (req, res, next) => {
     resInfo.sql3 = await executeQuery(
       'SELECT p.product_id, p.product_state, MAX(bidding_money), p.car_id, p.car_img, cars.car_name FROM products as p, biddings, cars WHERE p.product_state IN(2, 3) AND p.user_id = ? AND p.product_id = biddings.product_id AND cars.car_id = p.car_id GROUP BY p.product_id;',
       [3],
+      /** req.user.user_idに変更 */
     );
     /** user更新の為の情報一覧 */
     resInfo.sql4 = await executeQuery('SELECT * FROM users WHERE user_id = ?;', [req.user.user_id]);
@@ -39,15 +45,15 @@ router.post('/acount', async (req, res, next) => {
   await updateAccount(req, res, next);
 });
 /** カード情報変更 */
-// router.post("/card", async (req, res, next) => {
-
-// });
+router.post('/card', async (req, res, next) => {
+  await updateCard(req, res, next);
+});
 /** 住所変更 */
-// router.post("/address", async (req, res, next) => {
-
-// });
+router.post('/address', async (req, res, next) => {
+  await updateAddress(req, res, next);
+});
 /** 退会登録  */
-// router.post("/drop", async (req, res, next) => {
-
-// });
+router.post('/drop', async (req, res, next) => {
+  await updateDrop(req, res, next);
+});
 module.exports = router;
