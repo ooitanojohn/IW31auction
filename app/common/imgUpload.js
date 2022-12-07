d60d16aca983ed2710b74f7687b217b60bb730f6
xxi-dxx <i.daiki.xx@gmail.com>
2022-12-07 20:32:07 +0900

    🌟 feat: プロダクト一覧表示、ストック一覧表示、画像登録途中途中

--- app/common/imgUpload.js
+++ app/common/imgUpload.js
@@ -2,60 +2,55 @@ const express = require('express');
 
 const router = express.Router();
 const mysql = require('mysql');
-require('date-utils');
 const path = require('path');
 const fs = require('fs');
 
-function imgUpload(imageId, imageType) {
-  const mysqlSetting = {
-    host: '',
-    user: '',
-    password: '',
-    database: 'auction',
-  };
-  //
-  let imgPath = '';
-  let sql = '';
-  if (imageType === 'productMain') {
-    imgPath = './src/product/top';
-  } else if (imageType === 'productDetail') {
-    imgPath = './src/product/detail';
-  } else {
-    imgPath = './src/user';
-  }
-  if (imageType === 'productMain') {
-    sql = 'insert into テーブル名 set ?';
-  } else if (imageType === 'productDetail') {
-    sql = 'insert into テーブル名 set ?';
-  } else {
-    sql = 'insert into テーブル名 set ?';
-  }
-  // 画像がPOSTされた時の処理
-  router.post('/', (req, res) => {
-    // 変数準備
-    const id = imageId; // 今回変更したいDBのカラムID
-    if (imageType === 'productMain') {
-      // 商品情報を登録したい場合
-      // 画像ファイル（アイコン）の拡張子を取り出し、時間とsession変数で名前をつけ直す。これによりuserが全く同じ名前のファイルをアップしてきても大丈夫
-      const iconExt = path.extname(req.files.icon.name);
-      const newIconname = id + iconExt;
-      // 画像ファイルを保存するパスを設定
-      const targetPathI = imgPath + newIconname;
-      // ファイルをサーバーに保存した後、ファイル名をDBの保存、今後ファイルを取り出す際はDBから名前を取ってきてサーバーに保存してある画像ファイルとひもづける
-      fs.writeFile(targetPathI, req.files.icon.data, (err) => {
-        if (err) {
-          throw err;
-        } else {
-          const data = { icon: newIconname };
-          const connection = mysql.createConnection(mysqlSetting);
-          connection.connect();
-          connection.query(sql, data, () => {
-            res.redirect('/mypage');
-          });
-          connection.end();
-        }
-      });
-    }
-  });
-}
-module.exports = imgUpload;
+xpress = require('express');
+
+//*** 追加1 ここから***//
+const multer = require('multer');
+var storage = multer.diskStorage({
+  //ファイルの保存先を指定(ここでは保存先は./public/images)
+  //Express4の仕様かなんかで画像staticなファイルを保存するときはpublic/以下のフォルダに置かないとダメらしい
+  //詳しくは express.static public でググろう！
+  destination: function (req, file, cb) {
+    cb(null, './public/images/');
+  },
+  //ファイル名を指定
+  //ここでは image.jpg という名前で保存
+  filename: function (req, file, cb) {
+    cb(null, 'image.jpg');
+  },
+});
+
+var upload = multer({ storage: storage });
+// /**
+//  *
+//  * @param {*} imageObject 画像のオブジェクトが格納されている  最初がIDでそれ以降はファイル
+//  * @param {*} imageType 画像の種類　ユーザーと車が存在
+//  */
+// const imgUpload = (imageObject, imageType) =>{
+//   const array = Object.values(imageObject);
+//   const dir = '/src/'+imageType+'/'+array[0];
+//   if (!fs.existsSync(dir)) {
+//     fs.mkdir(dir, { recursive: true }, err => {
+//       if(err) console.log(err);
+//   });
+//   }
+//   for(let i =1 ;i < array.length; i++){
+//     //拡張子を取り出してファイルをリネーム
+//     var icon_ext = path.extname(array[i]);
+//     var new_iconname = array[0]+'_'+array[i]+icon_ext;
+//     //画像ファイルを保存するパスを設定
+//     var target_path_i = dir+'/'+new_iconname;
+//     fs.writeFile(target_path_i,imageObject.files.icon.data,(err) => {
+//       if(err){
+//           throw err
+//       }else{
+//           var data = {'icon':new_iconname};
+//       }
+//     });
+//   };
+//   return data;
+// };
+module.exports = { imgUpload };
--- app/controller/admin/productsManagementRouter.js
+++ app/controller/admin/productsManagementRouter.js
@@ -1,6 +1,59 @@
 const express = require('express');
-
 const router = express.Router();
+const formData = require('express-form-data');
+const path = require('path');
+const fs = require('fs');
+
+//*** 追加1 ここから***//
+var multer = require('multer');
+const storage = multer.diskStorage({
+  //ファイルの保存先を指定
+  //Express4の仕様かなんかで画像staticなファイルを保存するときはpublic/以下のフォルダに置かないとダメらしい
+  //詳しくは express.static public でググろう！
+  destination: function (req, file, cb) {
+    cb(null, '../../common/public/src/car');
+  },
+  //ファイル名を指定
+  //ここでは image.jpg という名前で保存
+  filename: function (req, file, cb) {
+    cb(null, 'image.jpg');
+  },
+});
+var upload = multer({ storage: storage });
+
+// ファイルアップロードに利用
+//router.use(formData.parse({ uploadDir: path.join(__dirname, '../../common/public/src/car'), autoClean: true }));
+
+// これを指定すると、サイズが0のファイルはfilesから削除してくれる
+// router.use(formData.format());
+/** 出品管理 product_tbl一覧表示 */
+router.get('/admin/products/1', async (req, res, next) => {
+  /** resに渡す情報とSQLモジュールの読み込み */
+  const resInfo = httpRapper(req);
+  /** paging */
+  const limit = 5;
+  const offset = paginate(5, 1);
+  debug(offset);
+  /** 画像データは10個。json化してinsertする */
+  try {
+    if (req.query === 'search') {
+      /** 絞り込み */
+      resInfo.sql = await executeQuery('SELECT * FROM `products` LIMIT ? OFFSET ?;', [
+        limit,
+        offset,
+      ]);
+    }
+    /** ここに処理を記述 */
+    resInfo.sql = await executeQuery(
+      'SELECT * FROM `products` WHERE `product_state` = 1 LIMIT ? OFFSET ?;',
+      [limit, offset],
+    );
+    debug(resInfo.sql);
+    res.render('admin/productsTable.ejs', { ejsRender: resInfo });
+  } catch (err) {
+    next(err);
+  }
+});
 /**
  * 出品管理 router + controller
  */
@@ -10,9 +63,40 @@ const debug = require('debug')('http:products');
 const { executeQuery, beginTran } = require('../../module/mysqlPool');
 const { httpRapper } = require('../../common/httpRapper');
 const { paginate } = require('../../common/paginate');
+// const { imgUpload }= require('../../common/imgUpload');
+/** carStock_tbl一覧表示
+ * 画像登録フォーム を書くリストの下につけておく
+ */
+router.get('/insert', async (req, res, next) => {
+  const stockInfo = httpRapper(req);
+  try {
+    stockInfo.sql = await executeQuery('SELECT * FROM `carstocks` WHERE `stock_state` = 0;');
+    // throw new Error("エラーテスト");
+    res.render('admin/carStock.ejs', { ejsRender: stockInfo });
+  } catch (err) {
+    next(err);
+  }
+});
+/** 商品登録
+ * carstocksテーブルからproductテーブルに登録。画像登録も行う
+ */
+router.post('/regist', upload.array('multi-files'), async (req, res, next) => {
+  res.header('Content-Type', 'text/plain;charset=utf-8');
+  const tran = await beginTran();
+  const stockInfo = httpRapper(req);
 
+  try {
+    stockInfo.sql = await tran.query(`SELECT * FROM carstocks WHERE stock_id = ?`, [req.body.id]);
+    //const img = imgUpload(req.files);
+    // throw new Error("エラーテスト");
+    await tran.commit();
+  } catch (err) {
+    await tran.rollback();
+    next(err);
+  }
+  res.end('OK');
+});
 /** 出品管理 product_tbl一覧表示 */
-/** 詳細 slider modalで表示 */
 router.get('/:page', async (req, res, next) => {
   /** resに渡す情報とSQLモジュールの読み込み */
   const resInfo = httpRapper(req);
@@ -24,69 +108,59 @@ router.get('/:page', async (req, res, next) => {
   try {
     if (req.query === 'search') {
       /** 絞り込み */
-      resInfo.sql = await executeQuery('SELECT * FROM `users` LIMIT ? OFFSET ?;', [limit, offset]);
+      resInfo.sql = await executeQuery('SELECT * FROM `products` LIMIT ? OFFSET ?;', [
+        limit,
+        offset,
+      ]);
     }
     /** ここに処理を記述 */
-    resInfo.sql = await executeQuery('SELECT * FROM `users` LIMIT ? OFFSET ?;', [limit, offset]);
-    debug(resInfo.sql);
-    res.render('admin/top.ejs', { ejsRender: resInfo });
-  } catch (err) {
-    next(err);
-  }
-});
-
-/** 出品登録画面での更新、論理削除処理 (form) */
-router.post('/:productId', async (req, res, next) => {
-  /** resに渡す情報とSQLモジュールの読み込み */
-  const tran = await beginTran();
-  try {
-    await tran.query(
-      `UPDATE user_tbl
-      SET card_number=?, card_key=?, user_state=?
-      WHERE user_id=?`,
-      [3540000000000001, 555, 1, 1],
+    resInfo.sql = await executeQuery(
+      'SELECT * FROM `products` WHERE `product_state` = 1 LIMIT ? OFFSET ?;',
+      [limit, offset],
     );
-    // throw new Error("エラーテスト");
-    await tran.commit();
-    res.end('OK');
+    debug(resInfo.sql);
+    res.render('admin/productsTable.ejs', { ejsRender: resInfo });
   } catch (err) {
-    await tran.rollback();
     next(err);
   }
 });
 
-/** carStock_tbl一覧表示
- * 画像登録フォーム×10 を書くリストの下につけておく
- */
-router.get('/insert', async (req, res, next) => {
-  const tran = await beginTran();
-  try {
-    await tran.query(`INSERT`, []);
-    // throw new Error("エラーテスト");
-    await tran.commit();
-    res.end('OK');
-  } catch (err) {
-    await tran.rollback();
-    next(err);
-  }
-});
+// /** 出品登録画面での更新、論理削除処理 (form) */
+// router.post('/:productId', async (req, res, next) => {
+//   /** resに渡す情報とSQLモジュールの読み込み */
+//   const tran = await beginTran();
+//   try {
+//     await tran.query(
+//       `UPDATE user_tbl
+//       SET card_number=?, card_key=?, user_state=?
+//       WHERE user_id=?`,
+//       [3540000000000001, 555, 1, 1],
+//     );
+//     // throw new Error("エラーテスト");
+//     await tran.commit();
+//     res.end('OK');
+//   } catch (err) {
+//     await tran.rollback();
+//     next(err);
+//   }
+// });
 
-/** carStock_tbl一覧表示
- * 画像登録フォーム×10 を書くリストの下につけておく
- * checkbox 選択 + 追加情報で 新規登録
- *
- */
-router.post('/insert', async (req, res, next) => {
-  const tran = await beginTran();
-  try {
-    await tran.query(`INSERT`, []);
-    // throw new Error("エラーテスト");
-    await tran.commit();
-    res.end('OK');
-  } catch (err) {
-    await tran.rollback();
-    next(err);
-  }
-});
+// /** carStock_tbl一覧表示
+//  * 画像登録フォーム×10 を書くリストの下につけておく
+//  * checkbox 選択 + 追加情報で 新規登録
+//  *
+//  */
+// router.post('/insert', async (req, res, next) => {
+//   const tran = await beginTran();
+//   try {
+//     await tran.query(`INSERT`, []);
+//     // throw new Error("エラーテスト");
+//     await tran.commit();
+//     res.end('OK');
+//   } catch (err) {
+//     await tran.rollback();
+//     next(err);
+//   }
+// });
 
 module.exports = router;
