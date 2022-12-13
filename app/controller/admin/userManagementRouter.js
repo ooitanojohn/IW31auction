310391d6ac5f83d56f02d6fa346bc5f84915b14a
nakata2002 <ookinaki12@gmail.com>
2022-12-13 14:35:07 +0900

    🧪 test:

--- app/controller/admin/userManagementRouter.js
+++ app/controller/admin/userManagementRouter.js
@@ -6,23 +6,41 @@ const router = express.Router();
  */
 /** 必要module読み込み */
 /** resに渡す情報とSQLモジュールの読み込み */
+const debug = require('debug')('http:usermanage');
 const debugMySQL = require('debug')('MySQL:user');
 const { executeQuery, beginTran } = require('../../module/mysqlPool');
 const { httpRapper } = require('../../common/httpRapper');
+const { paginate } = require('../../common/paginate');
 
-/** 会員管理 一覧表示 */
+/** 会員+-管理 一覧表示 */
 /** 詳細 slider modalで表示 */
-router.get('/', async (req, res, next) => {
+router.get('/:page', async (req, res, next) => {
   /** resに渡す情報とSQLモジュールの読み込み */
   const resInfo = httpRapper(req);
+  const limit = 5;
+  let data;
+  let offset;
+  resInfo.maxpage = 0;
+  debug(offset);
   try {
     debugMySQL(resInfo);
+    /** リンクがクリックされた時の処理 */
+    data = await executeQuery('SELECT count(*) as count FROM `users` ORDER BY `user_id`');
+    resInfo.maxpage = Math.ceil(data[0].count / limit);
+    resInfo.now = Math.max(req.params.page, 1); /** resInfo.nowが１未満になるのを防ぐ */
+    resInfo.now = Math.min(
+      resInfo.maxpage,
+      resInfo.now,
+    ); /** resInfo.nowが($maxpage+1)以上になるのを防ぐ */
+    offset = paginate(limit, resInfo.now);
+
     /** ここに処理を記述 */
-    resInfo.sql = await executeQuery('SELECT * FROM `users` ORDER BY `user_id` ASC').catch(
-      (err) => {
-        throw new Error(err);
-      },
-    );
+    resInfo.sql = await executeQuery(
+      'SELECT * FROM `users` ORDER BY `user_id` ASC LIMIT ? OFFSET ?;',
+      [limit, offset],
+    ).catch((err) => {
+      throw new Error(err);
+    });
     res.render('admin/userManagement.ejs', { ejsRender: resInfo });
   } catch (err) {
     next(err);
@@ -31,6 +49,7 @@ router.get('/', async (req, res, next) => {
 
 /** 出品登録画面での垢バン 論理削除処理 (form) */
 router.post('/', async (req, res, next) => {
+  debug(req.body);
   const tran = await beginTran();
   const resInfo = httpRapper(req);
   try {
@@ -38,7 +57,6 @@ router.post('/', async (req, res, next) => {
     resInfo.user_state = await executeQuery('SELECT * FROM `users` WHERE user_id = ?', [
       req.body.upbutton,
     ]);
-    // console.log(resInfo.user_state[0]['user_state']);
     if (resInfo.user_state[0].user_state === 0) {
       resInfo.user_state[0].user_state = 2;
     } else {
@@ -57,13 +75,12 @@ router.post('/', async (req, res, next) => {
         throw new Error(err);
       });
     await tran.commit();
-
-    resInfo.sql = await executeQuery('SELECT * FROM `users` ORDER BY `user_id` ASC');
-    res.render('admin/userManagement.ejs', { ejsRender: resInfo });
+    // res.redirect('admin/userManagement.ejs', { ejsRender: resInfo });
   } catch (err) {
     await tran.rollback();
     next(err);
   }
+  res.redirect(301, `/admin/users/${req.body.now}`);
 });
 
 /**
