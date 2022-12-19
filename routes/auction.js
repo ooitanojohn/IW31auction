e1bc4f0c5148da2305120597ec2be7da715bfd28
Ouki74531 <ouki39741206@gmail.com>
2022-12-19 16:28:55 +0900

    110-オークション一覧 auction.js更新しました

--- routes/auction.js
+++ routes/auction.js
@@ -5,9 +5,15 @@ const router = express.Router();
 const { httpRapper } = require('../app/common/httpRapper');
 
 /* オークション一覧表示 */
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   const resInfo = httpRapper(req);
-  res.render('auction', { ejsRender: resInfo });
+  try{
+    resInfo.sql = await executeQuery(
+      "SELECT products.product_id, cars.car_name, makers.maker_name, products.start_price,product.start_time products.end_time FROM products INNER JOIN cars ON products.car_id = cars.car_id INNER JOIN makers ON cars.maker_id = makers.maker_id WHERE products.start_time <= NOW() AND NOW() <= products.end_time;",);
+    res.render('auction.ejs', { ejsRender: resInfo });
+  }catch(err){
+    nextTick(err);
+  }
 });
 
 /** オークション  */
